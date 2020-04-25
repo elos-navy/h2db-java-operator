0e1dffa3c7f50f879fbe2c0a569ab633b38ddebb
Lukas Smiga <donkuk@localhost.localdomain>
2020-04-25 20:44:45 +0200

    Initial mvn project quickstart.

---
+++ h2db-operator/src/main/java/cz/elostech/h2db-operator/App.java
@@ -0,0 +1,13 @@
+package cz.elostech.h2db-operator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ h2db-operator/src/test/java/cz/elostech/h2db-operator/AppTest.java
@@ -0,0 +1,20 @@
+package cz.elostech.h2db-operator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
