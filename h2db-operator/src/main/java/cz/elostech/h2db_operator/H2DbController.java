ac5bb826cac754346ff41fb27a4309ed44e1ae8c
Lukas Smiga <lukas.smiga@elostech.cz>
2020-09-12 01:39:19 +0200

    fix

--- h2db-operator/src/main/java/cz/elostech/h2db_operator/H2DbController.java
+++ h2db-operator/src/main/java/cz/elostech/h2db_operator/H2DbController.java
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
 import io.fabric8.kubernetes.api.model.*;
+import io.fabric8.kubernetes.api.model.apiextensions.*;
 import io.fabric8.kubernetes.api.model.apps.Deployment;
 import io.fabric8.kubernetes.api.model.apps.DoneableDeployment;
 import io.fabric8.kubernetes.client.KubernetesClient;
